21a39babcaaa3e69ce99cde6804df716218bef39
huykt1988 <huypham.cat@gmail.com>
2025-09-05 00:13:44 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,2 +1,11 @@
-export default (req, res) => res.json({msg:"Hello n8n 🤙"})
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    return res.status(200).json({
+      msg: "Webhook OK ✅",
+      body: req.body,
+    });
+  }
+  res.status(200).json({ msg: "Send a POST request!" });
+}
+
 
