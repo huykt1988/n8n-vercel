26d515b46f4b80dc5f7fb8edd4b3bb1cc022c462
huykt1988 <huypham.cat@gmail.com>
2025-09-05 00:11:14 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,1 +1,9 @@
 export default (req, res) => res.json({msg:"Hello n8n 🤙"})
+{
+  "name": "n8n-webhook-test",
+  "version": "1.0.0",
+  "scripts": {
+    "start": "node api/webhook.js",
+    "vercel-build": "echo 'Skipping build step'"
+  }
+}
